5fd47bf47fb44037e17f5fb2869ff47b533b1e77
davidjesus2414 <david.jesus2414@gmail.com>
2026-07-23 19:37:20 +0000

    Completei o nivel Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,19 +1,74 @@
 #include <stdio.h>
 
-void movertorre(int casas){
-    if(casas > 0) {
+
+void moverbispo(int casasbispo) {
+    if(casasbispo > 0) {
+        printf("O Bispo, moveu uma casa para Cima!\n");
+        printf("O Bispo, moveu uma casa para Direita!\n");
+        moverbispo(casasbispo - 1);
+    }
+}
+
+void moverrainha(int casasrainha) {
+    if(casasrainha > 0) {
+        printf("A Rainha, moveu uma casa para Esquerda!\n");
+        moverrainha(casasrainha - 1);
+    }
+}
+
+void movertorre(int casastorre) {
+    if(casastorre > 0) {
         printf("A Torre, moveu uma casa para Direita!\n");
-        movertorre(casas - 1);
+        movertorre(casastorre - 1);
     }
 }
 
 int main() {
     
-    int movertorre = 5;
+    //VARIAVEIS    
 
-    return 0;
-}
+    int casastorre = 5;
+    int casasrainha = 8;
+    int casasbispo = 5;
+    int cavalo = 1;
 
+    
+    //MOVIMENTACAO DAS PEÇAS
+
+    printf("\n========MOVIMENTACAO DA TORRE========\n");
+    printf("\n");
+
+    movertorre(casastorre);
+    printf("\nA TORRE, MOVEU %d CASAS PARA DIREITA! \n", casastorre);
+
+    printf("\n========MOVIMENTACAO DA RAINHA========\n");
+    printf("\n");
+
+    moverrainha(casasrainha);
+    printf("\nA RAINHA, MOVEU %d CASAS PARA ESQUERDA! \n", casasrainha);
+
+    printf("\n========MOVIMENTACAO DO BISPO========\n");
+    printf("\n");
+
+    moverbispo(casasbispo);
+    printf("\nO BISPO, MOVEU %d CASAS PARA DIAGONAL DIREITA CIMA! \n", casasbispo);
+    printf("\n");
+
+    printf("\n========MOVIMENTACAO DA CAVALO========\n");
+    printf("\n");
+
+    while (cavalo--){
+
+        for(int i = 0; i < 2; i++){
+            printf("O Cavalo andou para Cima\n");
+        }
+        printf("Cavalo andou para direita\n");
+
+    printf("\nO CAVALO ANDOU EM L PARA CIMA!\n");
+    }
 
 
     
+ 
+    return 0;
+}
