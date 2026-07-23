31bfc272dd76d6a41ec6a435468f10d3f02170a8
davidjesus2414 <david.jesus2414@gmail.com>
2026-07-23 19:37:06 +0000

    Completei o Nivel Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,47 +1,19 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+void movertorre(int casas){
+    if(casas > 0) {
+        printf("A Torre, moveu uma casa para Direita!\n");
+        movertorre(casas - 1);
+    }
+}
 
 int main() {
     
-    int torre,bispo,rainha;
-    int cavalo = 1;
-
-    printf("\n========MOVIMENTACAO DA TORRE========\n");
-
-
-    for (torre = 0; torre < 5; torre++){
-            printf("A Torre, moveu uma casa para Direita!\n");
-        }
-        printf("\n### A Torre Andou 5 Casas para Direita! ###\n");
-
-    printf("\n========MOVIMENTACAO DO BISPO========\n");
-
-    for (bispo = 0; bispo < 5; bispo++){
-        printf("Bispo andou uma casa para cima e depois uma casa para Direita!\n");
-    }
-        printf("\n### O Bispo Andou 5 Casas, Na diagonal para Direita (Cima)! ###\n");
- 
-    printf("\n========MOVIMENTACAO DA RAINHA========\n");
-
-    for (rainha = 0; rainha < 8; rainha++){
-        printf("A Rainha, moveu uma casa para Esquerda!\n");
-    }
-        printf("\n### A Rainha Andou 8 casas para Esquerda! ###\n");
-
-    printf("\n========MOVIMENTACAO DO CAVALO========\n");
-
-    while (cavalo--){
-
-        for(int i = 0; i < 2; i++){
-            printf("O Cavalo andou para Cima\n");
-        }
-        printf("Cavalo andou para direita\n");
-
-    printf("\n### O Cavalo Andou 2 casas para cima e 1 para direita formando um L (Cima)! ###\n");
-    }
+    int movertorre = 5;
 
     return 0;
 }
+
+
+
+    
